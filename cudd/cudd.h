5a5839768f0e64e60c1268deefa68c48b241ac7b
Miquel Ramírez <miquel.ramirez@gmail.com>
2022-11-16 11:26:56 +1100

    Alvaro Torralba's modifications to Cudd
     - Corrected confusion between `Add` and `Bdd`

--- cudd/cudd.h
+++ cudd/cudd.h
@@ -662,7 +662,6 @@ extern int Cudd_bddUnbindVar(DdManager *dd, int index);
 extern int Cudd_bddVarIsBound(DdManager *dd, int index);
 extern DdNode * Cudd_addExistAbstract(DdManager *manager, DdNode *f, DdNode *cube);
 extern DdNode * Cudd_addUnivAbstract(DdManager *manager, DdNode *f, DdNode *cube);
-extern DdNode * Cudd_addUnivAbstractLimit(DdManager *manager, DdNode *f, DdNode *cube, unsigned int limit);
 extern DdNode * Cudd_addOrAbstract(DdManager *manager, DdNode *f, DdNode *cube);
 extern DdNode * Cudd_addApply(DdManager *dd, DD_AOP op, DdNode *f, DdNode *g);
 extern DdNode * Cudd_addPlus(DdManager *dd, DdNode **f, DdNode **g);
@@ -729,6 +728,7 @@ extern DdNode * Cudd_bddExistAbstract(DdManager *manager, DdNode *f, DdNode *cub
 extern DdNode * Cudd_bddExistAbstractLimit(DdManager * manager, DdNode * f, DdNode * cube, unsigned int limit);
 extern DdNode * Cudd_bddXorExistAbstract(DdManager *manager, DdNode *f, DdNode *g, DdNode *cube);
 extern DdNode * Cudd_bddUnivAbstract(DdManager *manager, DdNode *f, DdNode *cube);
+extern DdNode * Cudd_bddUnivAbstractLimit(DdManager *manager, DdNode *f, DdNode *cube, unsigned int limit);
 extern DdNode * Cudd_bddBooleanDiff(DdManager *manager, DdNode *f, int x);
 extern int Cudd_bddVarIsDependent(DdManager *dd, DdNode *f, DdNode *var);
 extern double Cudd_bddCorrelation(DdManager *manager, DdNode *f, DdNode *g);
--- cudd/cuddBddAbs.c
+++ cudd/cuddBddAbs.c
@@ -251,6 +251,44 @@ Cudd_bddUnivAbstract(
 } /* end of Cudd_bddUnivAbstract */
 
 
+
+DdNode *
+Cudd_addUnivAbstractLimit(
+        DdManager * manager,
+        DdNode * f,
+        DdNode * cube,
+        unsigned int limit)
+{
+    DdNode		*res;
+
+    unsigned int saveLimit = manager->maxLive;
+
+    if (bddCheckPositiveCube(manager, cube) == 0) {
+        (void) fprintf(manager->err,"Error:  Can only abstract cubes");
+        manager->errorCode = CUDD_INVALID_ARG;
+        return(NULL);
+    }
+
+    manager->maxLive = (manager->keys - manager->dead) + (manager->keysZ - manager->deadZ) + limit;
+
+    do {
+        manager->reordered = 0;
+        res = cuddAddUnivAbstractRecur(manager, Cudd_Not(f), cube);
+    } while (manager->reordered == 1);
+
+    manager->maxLive = saveLimit;
+
+    if (res != NULL) res = Cudd_Not(res);
+
+    if (manager->errorCode == CUDD_TIMEOUT_EXPIRED && manager->timeoutHandler) {
+        manager->timeoutHandler(manager, manager->tohArg);
+    }
+
+    return(res);
+
+} /* end of Cudd_addUnivAbstract */
+
+
 /**
   @brief Computes the boolean difference of f with respect to x.
 
